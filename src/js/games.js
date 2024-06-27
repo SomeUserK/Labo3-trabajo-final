0227236da221e1f43a9a65434ad7e61cb2d94279
Melina Sanchez <alguien4598@gmail.com>
2024-06-27 01:56:47 -0300

    arregle las estrellitass :DDD

--- src/js/games.js
+++ src/js/games.js
@@ -3,9 +3,6 @@ const games_container = document.getElementById('games-container');
 const txtBusqueda = document.getElementById('barraBusqueda');
 const btnBusqueda = document.getElementById('botonBusqueda');
 
-let _cooldown = false;
-let _page = 1;
-
 function HTMLreplacer(template, data) {
   let html = template;
   for (const key in data) {
@@ -29,15 +26,73 @@ async function loadHTMLAsString(url) {
   }
 }
 
-function isOnCooldown() {
-  if (_cooldown) return true;
-  _cooldown = true;
-  setTimeout(() => (_cooldown = false), 3.5 * 1000);
-  return false;
-}
+// Funcion para buscar juegos al hacer click en buscar
+btnBusqueda.addEventListener('click', async event => {
+  event.preventDefault();
 
-async function loadGames(search, page, max) {
+  const search = txtBusqueda.value;
+  const games = await obtainGames(search, 1, 20);
+
+  if (!games || !games.results?.length) {
+    games_container.innerHTML = HTMLreplacer(cardTemplate, {
+      name: 'No games found',
+      description: 'Try searching for a game',
+      image: './imagenes/no-image.png',
+    });
+    return;
+  }
+
+  const { results } = games;
+
+  const extraDataResults = await Promise.allSettled(
+    results.map(game => obtainAGameData(game.id))
+  );
+
+  const extraDatas = extraDataResults.map(result => result.value || null);
+
+  const cards = results.map((game, index) => {
+    if (!game) return '';
+    const gameData = extraDatas[index] || {};
+
+    const words = gameData.description?.split(' ') || [];
+
+    const htmlContent = `<div class="card">
+      <img src="${game.background_image}" class="card-img-top" alt="${
+      game.name
+    }" />
+      <div class="card-body text-center">
+        <h5 class="card-title">${game.name}</h5>
+        <a href="/game-detail.html" class="btn btn-primary">View More</a>
+      </div>
+      <section class="stars">
+        <div class="star">
+          <img src="/public/imagenes/dark-stars.png" alt="stars" />
+        </div>
+        <div class="progress">
+          <div
+            class="progress-bar bg-warning"
+            role="progressbar"
+            style="width: ${Math.floor((game.rating / 5) * 100) + '%'};"
+            aria-valuenow="25"
+            aria-valuemin="0"
+            aria-valuemax="100"
+          ></div>
+        </div>
+      </section>
+    </div>
+    `;
+    games_container.innerHTML = htmlContent;
+  });
+
+  window.scrollTo(0, 0);
+});
+
+(async () => {
+  window.scrollTo(0, 0);
+
   const cardTemplate = await loadHTMLAsString('./src/templates/game-card.html');
+  let page = 1;
+  let isLoadingState = 1;
 
   // TODO Que la carta default sea directamente un carta vacia cargando
   const defaultCard = HTMLreplacer(cardTemplate, {
@@ -45,21 +100,9 @@ async function loadGames(search, page, max) {
     description: 'Try searching for a game',
     image: './imagenes/no-image.png',
   });
-  if (page === 1) games_container.innerHTML = defaultCard;
+  games_container.innerHTML = defaultCard;
 
-  const genres = [
-    document.getElementById('action').checked ? 'action' : '',
-    document.getElementById('adventure').checked ? 'adventure' : '',
-    document.getElementById('sports').checked ? 'sports' : '',
-    document.getElementById('strategy').checked ? 'strategy' : '',
-    document.getElementById('simulation').checked ? 'simulation' : '',
-    document.getElementById('rpg').checked ? 'rpg' : '',
-    document.getElementById('puzzle').checked ? 'puzzle' : '',
-    document.getElementById('horror').checked ? 'horror' : '',
-    document.getElementById('racing').checked ? 'racing' : '',
-  ].filter(genre => genre !== '');
-
-  const firstGames = await obtainGames(search, page, max, genres);
+  const firstGames = await obtainGames('', page, 20);
 
   // En el caso de poder obtener las cartas, se insertan en el contenedor
   if (firstGames && firstGames.results?.length > 0) {
@@ -74,6 +117,7 @@ async function loadGames(search, page, max) {
 
       if (!game) return '';
       const gameData = extraDatas[index] || {};
+      if (index === 0) console.log(gameData);
 
       const words = gameData.description?.split(' ') || [];
       const stars_rating = Math.floor((game.rating / 5) * 100) + '%';
@@ -81,45 +125,57 @@ async function loadGames(search, page, max) {
       return HTMLreplacer(cardTemplate, {
         name: game.name,
         description: words.slice(0, 30).join(' ') + '...',
+        rating: game.rating,
         stars_rating: stars_rating,
         image: game.background_image,
-        id: game.id,
-        css: `.progress-bar[data-width] {width: ${Math.floor(
-          (game.rating / 5) * 100
-        )}%; }`,
       });
     });
 
-    if (page === 1) games_container.innerHTML = '';
-    games_container.innerHTML += cards.join('');
+    games_container.innerHTML = cards.join('');
+    setTimeout(() => (isLoadingState = 0), 3 * 1000);
+
+    // Evento para detectar si se llegó al final de la página
+    window.addEventListener('scroll', () => {
+      const posicionDesplazamiento = window.scrollY + window.innerHeight;
+      const alturaDocumento = document.documentElement.scrollHeight;
+
+      // Si no se llegó al final de la página o ya se está cargando, no hacer nada
+      if (!(posicionDesplazamiento >= alturaDocumento - 1) || isLoadingState)
+        return;
+
+      isLoadingState = 1;
+      console.log('Llegaste al final de la página');
+
+      setTimeout(() => (isLoadingState = 0), 5 * 1000);
+
+      // Cargar más juegos
+
+      obtainGames('', ++page, 20).then(async games => {
+        if (!games || !games.results?.length) return;
+
+        const extraDataResults = await Promise.allSettled(
+          games.results.map(game => obtainAGameData(game.id))
+        );
+        const extraDatas = extraDataResults.map(result => result.value || null);
+
+        const newCards = games.results.map((game, index) => {
+          if (!game) return '';
+          const gameData = extraDatas[index] || {};
+
+          const words = gameData.description_raw?.split(' ') || [];
+
+          return HTMLreplacer(cardTemplate, {
+            name: game.name,
+            description: words.slice(0, 36).join(' ') + '...',
+            rating: game.rating,
+            image: game.background_image,
+          });
+        });
+
+        games_container.innerHTML += newCards.join('');
+      });
+    });
   }
-}
 
-// Evento para detectar si se llegó al final de la página
-window.addEventListener('scroll', () => {
-  const posicionDesplazamiento = window.scrollY + window.innerHeight;
-  const alturaDocumento = document.documentElement.scrollHeight;
-
-  // Si no se llegó al final de la página o ya se está cargando, no hacer nada
-  if (!(posicionDesplazamiento >= alturaDocumento - 1) || isOnCooldown())
-    return;
-  console.log('Llegaste al final de la página');
-
-  // Cargar más juegos
-
-  loadGames(txtBusqueda.value, ++_page, 20);
-});
-
-// Funcion MAIN Auto Ejecutable
-(async () => {
-  window.scrollTo(0, 0);
-
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const search = urlParams.get('search') || '';
-
-  if (search) txtBusqueda.value = search;
-
-  isOnCooldown();
-  loadGames(search, 1, 20);
+  //
 })();
