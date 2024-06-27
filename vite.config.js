3bc40bfe8f50122e02c821c9fbc9c93608a433c8
TheNochtgamer <61437465+TheNochtgamer@users.noreply.github.com>
2024-06-27 09:24:55 -0300

    Añadi la configuracion de vite

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [],
+  build: {
+    rollupOptions: {
+      input: {
+        main: '/index.html',
+        page1: '/games.html',
+        page2: '/game-detail.html',
+        // Añade más páginas según sea necesario
+      },
+    },
+  },
+});
